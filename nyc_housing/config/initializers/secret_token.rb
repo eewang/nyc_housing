fb373fb0d5ce4e3cd25d76a6ce0a838df51c664a
janeeats <jane.vora@gmail.com>
2013-07-31 20:21:57 -0400

    set up models and associations

---
+++ nyc_housing/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NycHousing::Application.config.secret_token = 'f8dd2cc5d1a4df8e09b5cad6fbb05abef7fd08da537c27e8d12b7b543111943bacb2f7f8401d307f9f8073aaa22ae9201b4877c1ff6cd606e11e3c596549b7ae'
