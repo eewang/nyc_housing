9724f259be899ca463f4e8a126ec4ac8809836a3
Eugene Wang <eugene.wang.77@gmail.com>
2013-08-06 19:49:36 -0400

    adds seeds file

--- nyc_housing/db/seeds.rb
+++ nyc_housing/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+	b = Building.new({
+		:address => "#{i} Main Street, New York NY",
+		:building_structure => "walk-up",
+		:leasing_company => "Flatiron School",
+		:description => "Apartment building",
+		:pets_allowed => false,
+		:phone_number => "555-555-5555",
+		:year_built => 2013
+		})
+	puts "Building #{i} created!"
+	b.save!
+	3.times do |j|
+		a = Apartment.new({
+			:bedrooms => j,
+			:bathrooms => 1,
+			:building_id => b.id,
+			:unit_number => j,
+			:size => "800"
+			})
+		a.save!
+		puts "Apartment #{i}-#{j} created!"
+	end
+end
